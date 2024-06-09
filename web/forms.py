5bbaa805966c0312fbf210602274f2da02a6d2a3
ValeriaPFR <ValeriaPFR@users.noreply.github.com>
2024-06-09 12:10:00 -0400

    -----

--- web/forms.py
+++ web/forms.py
@@ -7,7 +7,22 @@ class ContactFormForm(forms.Form):
     message = forms.CharField(label='message' )
     
 
+# class LoginForm(forms.Form):
+#     username = forms.CharField(max_length= 64, label="Nombre")
+#     password = forms.CharField(label='Password', widget=forms.PasswordInput)
+#     email = forms.EmailField(label = 'Email')
+
 class LoginForm(forms.Form):
-    username = forms.CharField(max_length= 64, label="Nombre")
-    password = forms.CharField(label='Password', widget=forms.PasswordInput)
-    email = forms.EmailField(label = 'Email')
+    username = forms.CharField(
+        max_length=64,
+        label="Nombre",
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Nombre'})
+    )
+    password = forms.CharField(
+        label='Password',
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'})
+    )
+    email = forms.EmailField(
+        label='Email',
+        widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Email'})
+    )
